3acbecfbd97c23fc61b2f962811ad39f581b9beb
Rodrigo Henrique Correa <rodrigohenrique007@hotmail.com>
2024-07-21 18:35:54 -0300

    modified:   dags/__pycache__/rotina_principal.cpython-312.pyc 	modified:   dags/rotina_principal.py 	new file:   landing_zone/brewery_data.json 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-21T21\357\200\27231\357\200\27258.337316+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-21T21\357\200\27231\357\200\27258.337316+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-21T21\357\200\27231\357\200\27258.337316+00\357\200\27200/task_id=run_databricks_workflow/attempt=2.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-20T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-20T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-20T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=run_databricks_workflow/attempt=2.log" 	modified:   logs/dag_processor_manager/dag_processor_manager.log 	new file:   logs/scheduler/2024-07-21/rotina_principal.py.log

--- dags/rotina_principal.py
+++ dags/rotina_principal.py
@@ -1,34 +1,51 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 import requests
 import pandas as pd
+import json
 from azure.storage.blob import BlobServiceClient
 import os
 
 def fetch_and_upload_brewery_data():
     # Buscar variáveis do Airflow
-    azure_storage_connection_string = Variable.get('azure_storage_connection_string')
-    container_name = Variable.get('azure_blob_container_name')
-    blob_path = Variable.get('azure_blob_path')
+    azure_storage_connection_string = Variable.get("azure_storage_connection_string")
+    container_name = "row"
+    blob_path = "brewery_data.json"
 
     # Buscar dados da API
     url_base = "https://api.openbrewerydb.org/breweries"
     todos_os_dados = []
-    pagina = 1
+    registros_por_pagina = 50
 
+    # Verificar o número total de registros
+    resposta_inicial = requests.get(f'{url_base}?per_page=1')
+    if resposta_inicial.status_code == 200:
+        total_registros = resposta_inicial.json()
+        if 'Total-Count' in resposta_inicial.headers:
+            total_registros = int(resposta_inicial.headers['Total-Count'])
+            max_paginas = (total_registros // registros_por_pagina) + (1 if total_registros % registros_por_pagina > 0 else 0)
+        else:
+            # Método alternativo: Continuar buscando páginas até que uma página vazia seja retornada
+            max_paginas = None
+    else:
+        print("Erro ao buscar dados da API:", resposta_inicial.status_code)
+        return
+
+    pagina = 1
     while True:
-        resposta = requests.get(f'{url_base}?page={pagina}&per_page=50')
+        resposta = requests.get(f'{url_base}?page={pagina}&per_page={registros_por_pagina}')
         if resposta.status_code == 200:
             dados = resposta.json()
             if not dados:
                 break
             todos_os_dados.extend(dados)
             pagina += 1
+            if max_paginas and pagina > max_paginas:
+                break
         else:
-            print("Erro ao buscar dados da API:", resposta.status_code)
+            print(f"Erro ao buscar dados da API na página {pagina}: {resposta.status_code}")
             break
 
     if not todos_os_dados:
@@ -39,23 +56,46 @@ def fetch_and_upload_brewery_data():
     df = pd.DataFrame.from_dict(todos_os_dados)
     
     # Criar diretório 'landing_zone' se não existir
-    landing_zone_path = '/landing_zone'
+    landing_zone_path = r"C:\Users\Murillo Correa\airflow_Case_Beers\landing_zone"
     os.makedirs(landing_zone_path, exist_ok=True)
     
-    # Salvar CSV localmente na pasta 'landing_zone'
-    local_csv_path = os.path.join(landing_zone_path, 'brewery_data.csv')
-    df.to_csv(local_csv_path, index=False)
-    print(f"Dados salvos localmente em: {local_csv_path}")
+    # Salvar JSON localmente na pasta 'landing_zone'
+    local_json_path = os.path.join(landing_zone_path, 'brewery_data.json')
+    df.to_json(local_json_path, orient='records', lines=True)
+    print(f"Dados salvos localmente em: {local_json_path}")
 
     # Conectar ao Azure Blob Storage
     blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
 
-    # Fazer upload do CSV
-    with open(local_csv_path, "rb") as data:
+    # Fazer upload do JSON
+    with open(local_json_path, "rb") as data:
         blob_client.upload_blob(data, overwrite=True)
 
     print("Dados da API foram salvos no Blob Storage com sucesso!")
+
+
+def run_databricks_job():
+    # Buscar variáveis do Airflow
+    databricks_instance = Variable.get("databricks_instance")
+    token = Variable.get("databricks_token")
+    job_id = Variable.get("databricks_job_id")
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    base_url = f'https://{databricks_instance}/api/2.0/jobs/run-now'
+    payload = {
+        "job_id": job_id
+    }
+
+    response = requests.post(base_url, headers=headers, data=json.dumps(payload))
+    if response.status_code == 200:
+        print(f'Job com job_id {job_id} executado com sucesso.')
+    else:
+        print(f'Erro ao executar job com job_id {job_id}: {response.text}')
 
 default_args = {
     'owner': 'airflow',
@@ -64,36 +104,21 @@ default_args = {
 }
 
 with DAG(
-    'execute_databricks_notebooks',
+    'execute_databricks_workflow',
     default_args=default_args,
-    description='Executa notebooks no Databricks para processar dados da Open Brewery DB',
+    description='Executa um workflow no Databricks para processar dados da Open Brewery DB',
     schedule_interval='@daily',
     catchup=False,
 ) as dag:
     
-    run_fetch_and_upload_brewery_data = DatabricksRunNowOperator(
-        task_id='run_load_bronze_data',
-        databricks_conn_id='databricks_default',
-        notebook_task={'notebook_path': '/path/to/load_bronze_data'}
+    fetch_and_upload_brewery_data_task = PythonOperator(
+        task_id='fetch_and_upload_brewery_data',
+        python_callable=fetch_and_upload_brewery_data
     )
-        
-
-    run_load_bronze_data = DatabricksRunNowOperator(
-        task_id='run_load_bronze_data',
-        databricks_conn_id='databricks_default',
-        notebook_task={'notebook_path': '/path/to/load_bronze_data'}
+    
+    run_databricks_workflow_task = PythonOperator(
+        task_id='run_databricks_workflow',
+        python_callable=run_databricks_job
     )
 
-    run_transform_to_silver = DatabricksRunNowOperator(
-        task_id='run_transform_to_silver',
-        databricks_conn_id='databricks_default',
-        notebook_task={'notebook_path': '/path/to/transform_to_silver'}
-    )
-
-    run_create_gold_aggregation = DatabricksRunNowOperator(
-        task_id='run_create_gold_aggregation',
-        databricks_conn_id='databricks_default',
-        notebook_task={'notebook_path': '/path/to/create_gold_aggregation'}
-    )
-
-    run_load_bronze_data >> run_transform_to_silver >> run_create_gold_aggregation
+    fetch_and_upload_brewery_data_task >> run_databricks_workflow_task
