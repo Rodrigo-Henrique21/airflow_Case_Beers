61a2ffe667b31a8259f53fc884f7b739e63ce0e3
Rodrigo Henrique Correa <rodrigohenrique007@hotmail.com>
2024-07-22 21:41:19 -0300

    modified:   README.md 	modified:   dags/__pycache__/rotina_principal.cpython-312.pyc 	modified:   dags/rotina_principal.py 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-21T21\357\200\27255\357\200\27222.674718+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-21T21\357\200\27255\357\200\27222.674718+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-22T22\357\200\27246\357\200\27231.055486+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=manual__2024-07-22T22\357\200\27246\357\200\27231.055486+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	modified:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-20T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	modified:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-20T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-21T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-21T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-22T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=fetch_and_upload_brewery_data/attempt=1.log" 	new file:   "logs/dag_id=execute_databricks_workflow/run_id=scheduled__2024-07-22T00\357\200\27200\357\200\27200+00\357\200\27200/task_id=run_databricks_workflow/attempt=1.log" 	modified:   logs/dag_processor_manager/dag_processor_manager.log 	modified:   logs/scheduler/2024-07-21/rotina_principal.py.log 	new file:   logs/scheduler/2024-07-22/rotina_principal.py.log 	new file:   logs/scheduler/2024-07-23/rotina_principal.py.log 	new file:   references/.gitkeep 	new file:   references/img/Bev.png 	modified:   teste/teste.py 	modified:   dags/__pycache__/rotina_principal.cpython-312.pyc 	modified:   logs/scheduler/2024-07-23/rotina_principal.py.log

--- dags/rotina_principal.py
+++ dags/rotina_principal.py
@@ -1,14 +1,157 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
+from airflow.utils.email import send_email
+from datetime import timedelta
 import requests
 import pandas as pd
 import json
 from azure.storage.blob import BlobServiceClient
 import os
 
-def fetch_and_upload_brewery_data():
+# Definir variáveis de ambiente para Databricks
+DATABRICKS_INSTANCE = Variable.get("databricks_instance")
+DATABRICKS_TOKEN = Variable.get("databricks_token")
+
+# Nome do workflow que queremos verificar ou criar
+WORKFLOW_NAME = "Pipilene_api_ambev"
+
+# URL base da API REST do Databricks
+BASE_URL = f"https://{DATABRICKS_INSTANCE}/api/2.0"
+
+# Função para verificar se um workflow existe
+def verificar_workflow(workflow_name):
+    headers = {
+        "Authorization": f"Bearer {DATABRICKS_TOKEN}"
+    }
+    response = requests.get(f"{BASE_URL}/jobs/list", headers=headers)
+    if response.status_code == 200:
+        jobs = response.json().get("jobs", [])
+        for job in jobs:
+            if job["settings"]["name"] == workflow_name:
+                return True
+    return False
+
+# Função para criar um workflow
+def criar_workflow():
+    email_success = Variable.get("email_success")
+    email_failure = Variable.get("email_failure")
+    git_url = Variable.get("git_url")
+    
+    headers = {
+        "Authorization": f"Bearer {DATABRICKS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "name": WORKFLOW_NAME,
+        "email_notifications": {
+            "on_success": [email_success],
+            "on_failure": [email_failure],
+            "no_alert_for_skipped_runs": False
+        },
+        "webhook_notifications": {},
+        "notification_settings": {
+            "no_alert_for_skipped_runs": False,
+            "no_alert_for_canceled_runs": False
+        },
+        "timeout_seconds": 0,
+        "health": {
+            "rules": [
+                {
+                    "metric": "RUN_DURATION_SECONDS",
+                    "op": "GREATER_THAN",
+                    "value": 1800
+                }
+            ]
+        },
+        "max_concurrent_runs": 1,
+        "tasks": [
+            {
+                "task_key": "Load_Bronze_Data",
+                "run_if": "ALL_SUCCESS",
+                "notebook_task": {
+                    "notebook_path": "Load_row_to_DLT",
+                    "source": "GIT"
+                },
+                "existing_cluster_id": "0720-180306-v8yifq82",
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "notification_settings": {
+                    "no_alert_for_skipped_runs": False,
+                    "no_alert_for_canceled_runs": False,
+                    "alert_on_last_attempt": False
+                },
+                "webhook_notifications": {}
+            },
+            {
+                "task_key": "Treatment_to_silver",
+                "depends_on": [
+                    {
+                        "task_key": "Load_Bronze_Data"
+                    }
+                ],
+                "run_if": "ALL_SUCCESS",
+                "notebook_task": {
+                    "notebook_path": "Transform_to_silver",
+                    "source": "GIT"
+                },
+                "existing_cluster_id": "0720-180306-v8yifq82",
+                "max_retries": 1,
+                "min_retry_interval_millis": 3600000,
+                "retry_on_timeout": True,
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "notification_settings": {
+                    "no_alert_for_skipped_runs": False,
+                    "no_alert_for_canceled_runs": False,
+                    "alert_on_last_attempt": False
+                },
+                "webhook_notifications": {}
+            },
+            {
+                "task_key": "Treatment_to_gold",
+                "depends_on": [
+                    {
+                        "task_key": "Treatment_to_silver"
+                    }
+                ],
+                "run_if": "ALL_SUCCESS",
+                "notebook_task": {
+                    "notebook_path": "Transform_to_gold",
+                    "source": "GIT"
+                },
+                "existing_cluster_id": "0720-180306-v8yifq82",
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "notification_settings": {
+                    "no_alert_for_skipped_runs": False,
+                    "no_alert_for_canceled_runs": False,
+                    "alert_on_last_attempt": False
+                },
+                "webhook_notifications": {}
+            }
+        ],
+        "git_source": {
+            "git_url": git_url,
+            "git_provider": "gitHub",
+            "git_branch": "Databricks_dev"
+        },
+        "format": "MULTI_TASK",
+        "queue": {
+            "enabled": True
+        }
+    }
+    
+    response = requests.post(f"{BASE_URL}/jobs/create", headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        print(f"Workflow '{WORKFLOW_NAME}' criado com sucesso.")
+    else:
+        print(f"Erro ao criar workflow: {response.json()}")
+
+# Função para buscar e fazer upload dos dados da Brewery
+def buscar_e_fazer_upload_dados_brewery():
     # Buscar variáveis do Airflow
     azure_storage_connection_string = Variable.get("azure_storage_connection_string")
     container_name = "row"
@@ -56,7 +199,7 @@ def fetch_and_upload_brewery_data():
     df = pd.DataFrame.from_dict(todos_os_dados)
     
     # Criar diretório 'landing_zone' se não existir
-    landing_zone_path = r"C:\Users\Murillo Correa\airflow_Case_Beers\landing_zone"
+    landing_zone_path = r"/opt/airflow/landing_zone"
     os.makedirs(landing_zone_path, exist_ok=True)
     
     # Salvar JSON localmente na pasta 'landing_zone'
@@ -74,12 +217,11 @@ def fetch_and_upload_brewery_data():
 
     print("Dados da API foram salvos no Blob Storage com sucesso!")
 
-
-def run_databricks_job():
-    # Buscar variáveis do Airflow
-    databricks_instance = Variable.get("databricks_instance")
-    token = Variable.get("databricks_token")
-    job_id = Variable.get("databricks_job_id")
+# Função para executar um job no Databricks
+def executar_job_databricks():
+    databricks_instance = "adb-844462385794132.12.azuredatabricks.net"
+    token = "dapicd6b1aa85deaf7bc282c6aa47e2b8cab-3"
+    job_id = "968731268030929"
 
     headers = {
         'Authorization': f'Bearer {token}',
@@ -97,28 +239,56 @@ def run_databricks_job():
     else:
         print(f'Erro ao executar job com job_id {job_id}: {response.text}')
 
+# Função para enviar email em caso de falha
+def enviar_email_falha(context):
+    email_to = Variable.get("email_failure")
+    subject = f"Airflow Task Failed: {context['task_instance_key_str']}"
+    html_content = f"""
+    <p>Task: {context['task_instance_key_str']} failed</p>
+    <p>Dag: {context['dag'].dag_id}</p>
+    <p>Execution Time: {context['execution_date']}</p>
+    <p>Log URL: {context['task_instance'].log_url}</p>
+    """
+    send_email(email_to, subject, html_content)
+
+# Configurações padrão para o DAG
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
     'retries': 1,
+    'on_failure_callback': enviar_email_falha,
+    'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    'execute_databricks_workflow',
+    'executar_workflow_databricks',
     default_args=default_args,
     description='Executa um workflow no Databricks para processar dados da Open Brewery DB',
     schedule_interval='@daily',
     catchup=False,
 ) as dag:
     
-    fetch_and_upload_brewery_data_task = PythonOperator(
-        task_id='fetch_and_upload_brewery_data',
-        python_callable=fetch_and_upload_brewery_data
+    start_task = EmptyOperator(
+        task_id='start'
     )
     
-    run_databricks_workflow_task = PythonOperator(
-        task_id='run_databricks_workflow',
-        python_callable=run_databricks_job
+    buscar_e_fazer_upload_dados_brewery_task = PythonOperator(
+        task_id='buscar_e_fazer_upload_dados_brewery',
+        python_callable=buscar_e_fazer_upload_dados_brewery
+    )
+    
+    verificar_e_criar_workflow_task = PythonOperator(
+        task_id='verificar_e_criar_workflow',
+        python_callable=lambda: criar_workflow() if not verificar_workflow(WORKFLOW_NAME) else print(f"Workflow '{WORKFLOW_NAME}' já existe.")
+    )
+    
+    executar_workflow_databricks_task = PythonOperator(
+        task_id='executar_workflow_databricks',
+        python_callable=executar_job_databricks
+    )
+    
+    end_task = EmptyOperator(
+        task_id='end'
     )
 
-    fetch_and_upload_brewery_data_task >> run_databricks_workflow_task
+    start_task >> buscar_e_fazer_upload_dados_brewery_task >> verificar_e_criar_workflow_task >> executar_workflow_databricks_task >> end_task
