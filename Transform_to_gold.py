735842266eb82f01a913edd4c732bbe389a166e5
Rodrigo-Henrique21 <Rodrigo-Henrique21>
2024-07-25 20:22:04 +0000

    correcao save table

--- Transform_to_gold.py
+++ Transform_to_gold.py
@@ -8,23 +8,6 @@
 
 import os
 
-# Credenciais no Azure Key Vault e configuradas no Databricks Secrets
-storage_account_name = os.environ['storage_account_name']
-container_name = "gold"
-mount_point = "/mnt/gold"
-
-# Montando o armazenamento
-if not any(mount.mountPoint == mount_point for mount in dbutils.fs.mounts()):
-    dbutils.fs.mount(
-        source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net",
-        mount_point = mount_point,
-        extra_configs = {
-            f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net": dbutils.secrets.get(scope="keyvault", key="keystorage")
-        }
-    )
-
-# Define constantes e chaves de ambiente
-storage_account_name = os.environ['storage_account_name']
 
 # Criação do esquema 'gold' se não existir
 spark.sql("CREATE SCHEMA IF NOT EXISTS gold")
@@ -42,6 +25,7 @@ external_table_path = f"{mount_point}/brewery_data"
 df_gold.write\
          .format("delta")\
             .mode("overwrite")\
-                .partitionBy("estado", "cidade")\
+                .partitionBy("estado", "tipo_cervejaria")\
+                  .option("overwriteSchema", "true")\
                     .saveAsTable("gold.brewery_data")
 
